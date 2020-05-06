0145c4aeba643b41490f4fd75bd43a0bdae8f6b9
Nnadili Innocent <innoveran@gmail.com>
2020-05-06 12:00:29 +0100

    add the links for bootsrap and jquery

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aimone | Creative Agency</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
     <p>testing</p> 
     
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!--bootstrap JS -->
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
