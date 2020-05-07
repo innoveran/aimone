2cc2e98093aa68b5894378bc3385eecf56a33127
Nnadili Innocent <innoveran@gmail.com>
2020-05-07 11:00:35 +0100

    implement the favicon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aimone | Creative Agency</title>
+    <link rel="icon" type="image/jpeg" href="images/favicon.jpeg">
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
