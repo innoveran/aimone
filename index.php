2a313222d53def9c556b573e3d82435a6437f648
Nnadili Innocent <innoveran@gmail.com>
2020-05-13 05:00:19 +0100

    implement the mobile and medium device view for banner section

--- index.php
+++ index.php
@@ -6,59 +6,68 @@
     <title>Aimone | Creative Agency</title>
     <link rel="icon" type="image/jpeg" href="images/favicon.jpeg">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="//db.onlinewebfonts.com/c/ef6bdf5ef216552c7e9869841e891ca0?family=Arial+Rounded+MT+Bold" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-md navbar-light border-0">
-            <div class="container-fluid ">
-                <a class="navbar-brand" href="" style="padding-left: 55px;">
-                    <img src="images/logo/logo.png" id="salutelogo" alt="aimone logo" style="width:25%; height:43px;"> 
-                </a>
-                <button class="navbar-toggler" type="button" id="smsidebar" >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto buffer-bold ">
-                        <!-- Authentication Links -->
-                        <li class="nav-item">
-                            <a href="" class="nav-link portsol">ABOUT US</a>
-                        </li>
-                        <li class="nav-item action-btn-outline">
-                            <a href="" class="nav-link btn text-left">SERVICES</a>
-                        </li>
-                        <li class="nav-item action-btn-outline">
-                            <a href="" class="nav-link btn text-left">GALLERY</a>
-                        </li>
-                        <li class="nav-item action-btn-outline">
-                            <a href="" class="nav-link btn text-left">CONTACT</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-denger py-0 head-nav">
+        <div class="container">
+            <a class="navbar-brand  text-dark py-0 nav-anchor" href="/">
+                <img src="images/logo/logo.png" alt="Property Realty Logo" style="width:133.3px;height:70px">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon company-color"><i class="fas fa-bars "></i></span>
+            </button>
             
-        </nav>
-    </header>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="" class="nav-link btn text-left">ABOUT US</a>
+                    </li>
+                    <li class="nav-item action-btn-outline">
+                        <a href="" class="nav-link btn text-left">SERVICES</a>
+                    </li>
+                    <li class="nav-item action-btn-outline">
+                        <a href="" class="nav-link btn text-left">GALLERY</a>
+                    </li>
+                    <li class="nav-item action-btn-outline">
+                        <a href="" class="nav-link btn text-left">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> 
 
     <main>
         <section class="banner-section">
-            <div class="row mx-0">
-                <div class="col-md-7 banner-section-text">
-                    <h2 class="branding-text display-3">Branding Your Way</h2>
-                    <p class="">Lets help you communicate your brand values creatively</p>
-                    <button class="get-started px-4 py-2">Get Started</button>
+            <div class="row mx-0 d-block d-md-none banner-space-sm">
+                <div class="col-12">
+                    <h2 class="text-center banner-title-mobile">Branding Your Way</h2>
+                   
+                        <img src="images/banner/banner-image.png" alt="" style="width:100%; height:68%">
+                
+                    <p class="banner-description mt-3 text-center">Lets help you communicate your brand values creatively</p>
+                    <button class="get-started-mobile px-4 py-2">Get Started</button>
                 </div>
-                <div class="col-md-5 banner-section-img">
+            </div>
+            
+            <div class="row mx-0 d-sm-none d-md-flex">
+                <div class="col-md-7 banner-section-text">
+                    <h2 class="branding-text display-4">Branding Your Way</h2>
+                    <div class="col-md-5 banner-section-img d-block d-md-none">
+                        <img src="images/banner/banner-image.png" alt="" style="width:100%; height:100%">
+                    </div>
+                    <p class="banner-description mt-3">Lets help you communicate your brand values creatively</p>
+                    <button class="get-started px-4 py-2 mt-3">Get Started</button>
+                </div>
+                <div class="col-md-5 banner-section-img d-none d-md-block">
                     <img src="images/banner/banner-image.png" alt="" style="width:100%; height:100%">
                 </div>
             </div>
+            
+        </section>
+        <section>
+            
         </section>
     </main>
     
@@ -71,5 +80,7 @@
 
     <!--bootstrap JS -->
     <script src="js/bootstrap.js"></script>
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </body>
 </html>
